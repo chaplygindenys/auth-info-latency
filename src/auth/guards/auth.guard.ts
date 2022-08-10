1efd295d2f3eefbb378d2c5f2e5549ac78f02030
chaplygindenys <chaplygindenys@gmail.com>
2022-08-10 16:58:09 +0300

    add isPublic  to Guard

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class RefTokGuard extends AuthGuard('jwt') {
-  constructor() {
+export class AccTokGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
     super();
+  }
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
+
+    return super.canActivate(context);
   }
 }
