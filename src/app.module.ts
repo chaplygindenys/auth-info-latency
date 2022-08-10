d4aa59e2289fdb893c2a010f321c030b97b4a3a4
chaplygindenys <chaplygindenys@gmail.com>
2022-08-10 15:51:42 +0300

    module auth info

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { InfoModule } from './info/info.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, InfoModule],
   controllers: [AppController],
   providers: [AppService],
 })
