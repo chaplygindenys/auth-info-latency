6809b9ff8f96326c9e86fd8e4f3311653742de33
chaplygindenys <chaplygindenys@gmail.com>
2022-08-10 16:51:36 +0300

    guard -strategy-decorator

---
+++ src/auth/strategies/authJwt.strategy.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+
+type JwtPayload = {
+  id: string;
+};
+
+@Injectable()
+export class AccTokStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET_KEY,
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return payload;
+  }
+}
